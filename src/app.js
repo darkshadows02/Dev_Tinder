e25249a6770fe609c0d7c2f89af986b6699192e3
Suraj Kumar <20203048.it@gmail.com>
2025-12-29 10:44:40 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express=require("express");
+
+const app=express();
+app.use((req, res)=>{
+    res.send("hello from the server!")
+})
+app.use("hello", (req, res)=>{
+    res.send("hello from the server!")
+})
+
+app.listen(3000, ()=>{
+    console.log("server created sucessfully")
+});
