8d97ecb630f2a346cafaaa5574306aadaff995dc
Suraj Kumar <20203048.it@gmail.com>
2025-12-29 11:35:07 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 const express=require("express");
 
 const app=express();
-app.use((req, res)=>{
+
+
+app.get("/hello", (req, res)=>{
     res.send("hello from the server!")
 })
-app.use("hello", (req, res)=>{
-    res.send("hello from the server!")
+app.post("/hello", (req, res)=>{
+    console.log("post call")
+    res.send({firstname:"suraj", lastname:"kumar"})
 })
+app.delete("/hello", (req, res)=>{
+    console.log("delete call")
+    res.send("deleted save request")
+})
+
+
+// app.use((req, res)=>{
+//     res.send("hello from the server!")
+// })
+// app.use("hello", (req, res)=>{
+//     res.send("hello from the server!")
+// })
 
 app.listen(3000, ()=>{
     console.log("server created sucessfully")
