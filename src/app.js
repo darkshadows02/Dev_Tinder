9bd30a82e56ed95aaff8dc6fa0d810d5651cb387
Suraj Kumar <20203048.it@gmail.com>
2025-12-30 10:10:05 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,18 +2,31 @@ const express=require("express");
 
 const app=express();
 
+app.get('/user', (req, res, next)=>{
+    console.log("handeling the route user 1 !!")
+      next();
+},(req, res, next)=>{
+    console.log("handeling the route user 2 !!")
+      next();
+},(req, res, next)=>{
+    console.log("handeling the route user 3 !!")
+      next();
+},(req, res)=>{
+    console.log("handeling the route user 4 !!")
+    res.send("responce!!")
+})
 
-app.get("/hello", (req, res)=>{
-    res.send("hello from the server!")
-})
-app.post("/hello", (req, res)=>{
-    console.log("post call")
-    res.send({firstname:"suraj", lastname:"kumar"})
-})
-app.delete("/hello", (req, res)=>{
-    console.log("delete call")
-    res.send("deleted save request")
-})
+// app.get("/hello", (req, res)=>{
+//     res.send("hello from the server!")
+// })
+// app.post("/hello", (req, res)=>{
+//     console.log("post call")
+//     res.send({firstname:"suraj", lastname:"kumar"})
+// })
+// app.delete("/hello", (req, res)=>{
+//     console.log("delete call")
+//     res.send("deleted save request")
+// })
 
 
 // app.use((req, res)=>{
