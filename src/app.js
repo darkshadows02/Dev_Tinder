915d7bfb56aabaf88dc09250b08fa7cb1b79405f
Suraj Kumar <20203048.it@gmail.com>
2026-01-02 11:11:48 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -18,7 +18,25 @@ app.post("/signup", async (req, res)=>{
         }
        
 })
-
+//get user by email
+//checking for finding models in db for one
+app.get("/user", async (req, res)=>{
+       try{
+       const users=  await User.findOne({emailId:req.body.emailId})
+      res.send(users)
+    }catch(err){
+        res.status(400).send("error saving the user", err.message);
+       }
+})
+ // feed API - get/feed - get all the users from the database
+app.get("/feed", async(req, res)=>{
+       try{
+          const users=await User.find({})
+          res.send(users)
+       }catch(err){
+        res.status(400).send("error saving the user", err.message);
+       }
+})
 
 connectDb().then(()=>{
     console.log("cluster connected sucessfually...")
