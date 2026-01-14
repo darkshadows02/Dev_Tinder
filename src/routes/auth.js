036baa4c3cd1bff6ac60c3d6d016b92fa737a126
Suraj Kumar <20203048.it@gmail.com>
2026-01-14 23:13:12 +0530

    api request/send

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -25,12 +25,13 @@
 });
 authRouter.post("/login", async(req, res)=>{
     try{
-       const {emailId, password}=req.body;
+       const {emailId, password}=req.body;   
            const user=await User.findOne({emailId:emailId})
               
            if(!user){
-            throw new Error("  Invllid credentials")
+            throw new Error("Invllid credentials")
            }
+           console.log(req.body)
            const isPasswordValid=await user.validatePassword(password);
             
            if(isPasswordValid){
@@ -44,7 +45,7 @@ authRouter.post("/login", async(req, res)=>{
         res.status(400).send("ERROR"+" "+err.message);
     }
 })
- 
+
 authRouter.post("/logout", async (req, res)=>{
       res.cookie("token", null, {
         expires: new Date(Date.now()),
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,10 +1,26 @@
 const express=require("express");
 const {userauth}=require("../middlewares/auth")
-
+const ConncetionRequest =require("../models/connectionRequest")
 const  requestRouter=express.Router();
 
-requestRouter.post("/sendConnectionRequest",userauth, (req, res)=>{
-    res.send("send the connection request");   
+requestRouter.post("/request/send/:status/:toUserId",userauth, async (req, res)=>{
+     try{
+        const fromUserId=req.user._id;
+        const toUserId=req.params.toUserId;
+        const status=req.params.status;
+        const conncetionRequest=new ConncetionRequest({
+            fromUserId,
+            toUserId,
+            status,
+        });
+        const data= await conncetionRequest.save();
+        res.json({
+            message:"Connection Request Sent Sucessfully !!",
+            data,
+        })
+     }catch(err){
+        res.status(400).send("ERROR"+err.message)
+     }
 });
 
 module.exports=requestRouter;
