1f8bbc52f090d666845d683bda2542ef3c675fe8
Suraj Kumar <20203048.it@gmail.com>
2026-01-22 10:43:59 +0530

    user/connections api completed

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,8 @@ const ConncetionRequest = require("../models/connectionRequest");
 const userRouter=express.Router();
 
 
+const USER_SAFE_DATA="firstName lastName age gender about skill";
+
 //get all the pending request for the loggedIn user
 userRouter.get("/user/requests/received",userauth, async(req, res)=>{
      try{
@@ -11,9 +13,8 @@ userRouter.get("/user/requests/received",userauth, async(req, res)=>{
          const conncetionRequest=await ConncetionRequest.find({
             toUserId:loggedInUser._id,
             status:"interested",
-         }).populate(
-              "fromUserId","firstName lastName")
-              //["firstName", "lastName"] also we write like this
+         }).populate("fromUserId",USER_SAFE_DATA)
+               //["firstName", "lastName"] also we write like this
           res.json({
             message:"Data fetched sucessfully",
             data:conncetionRequest
@@ -21,6 +22,29 @@ userRouter.get("/user/requests/received",userauth, async(req, res)=>{
      }catch(err){
         res.status(400).send("ERROR"+err.message)
      }
+})
+userRouter.get("/user/connections", userauth,  async(req, res)=>{
+    try{
+        const loggedInUser=req.user;
+        const conncetionRequest=await ConncetionRequest.find({
+            $or:[
+                {toUserId:loggedInUser._id, status:"accepted"},
+                {fromUserId:loggedInUser._id, status:"accepted"},
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA);
+
+        const data=conncetionRequest.map((row)=>{
+                if(row.fromUserId.toString()===loggedInUser._id.toString()){
+                    return row.toUserId
+                }else{
+                    return row.fromUserId
+                }
+        })
+        res.json({data})
+    }catch(err){
+        res.status(400).send("ERROR"+err.message) 
+    }
 })
 
 
